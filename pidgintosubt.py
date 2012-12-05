477b09109e1e7731dd7fbaa23d8dbcab340c1790
Jay McClow <jay.mcclow.jr@gmail.com>
2012-12-05 12:45:01 -0800

    Adding a 30 second warning to beginning of film

--- pidgintosubt.py
+++ pidgintosubt.py
@@ -2,13 +2,13 @@
 from datetime import datetime
 import time
 import re
-def repl(string):
+def repl(string, begin):
     substring = re.findall('(\d{2}:\d{2}:\d{2})', string) 
     for i in substring:
         semicolon_split = i.split(':')
         dt_obj = datetime(2012, 11, 23, int(semicolon_split[0]), int(semicolon_split[1]), int(semicolon_split[2]))
         # Edit next line for epoch time offset from where you start to be 00:00:00
-        epoch_to_adjust = 24867
+        epoch_to_adjust = begin 
         epoch_date = int(dt_obj.strftime("%s")) - epoch_to_adjust 
         begin = time.strftime("%H:%M:%S,000", time.localtime(epoch_date))
         end = time.strftime("%H:%M:%S,000", time.localtime(epoch_date + 10))
@@ -19,8 +19,17 @@ def repl(string):
 #-------------------------------------------------
 # datetime object to string
 inf = open('Subtitles.txt')
+warning = inf.readline()
+stamp = re.match('\((\d{2}:\d{2}:\d{2})\)', warning)
+begin = stamp.group(0)
+begin_split = re.findall('(\d{2}:\d{2}:\d{2})', begin)
+for d in begin_split:
+    semic_split = d.split(':')
+    dt_obj = datetime(2012, 11, 23, int(semic_split[0]), int(semic_split[1]), int(semic_split[2]))
+    zero_dt_obj = datetime(2012, 11, 22, 23, 59, 30)
+    epoch_dt = int(dt_obj.strftime("%s")) - int(zero_dt_obj.strftime("%s")) 
 subtitle_num = 1 
 for line in inf:
     print subtitle_num
-    print re.sub('\((\d{2}:\d{2}:\d{2})\)', repl(line), line)
+    print re.sub('\((\d{2}:\d{2}:\d{2})\)', repl(line, epoch_dt), line)
     subtitle_num += 1
